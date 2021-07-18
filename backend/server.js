40333e75c2969ff461578dba820deca5285c2166
Andres Canchila <andresfelipecx@gmail.com>
2021-07-18 15:53:22 -0500

    sigin user

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 /* eslint-disable no-undef */
 const express = require('express');
+const mongoose = require('mongoose');
+
+const productRouter = require('./routers/productRouter');
+const userRouter = require('./routers/userRouter');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-const mongoose = require('mongoose');
-const productRouter = require('./routers/productRouter');
-const userRouter = require('./routers/userRouter');
 
 mongoose.connect (process.env.MONGODB_URL || 'mongodb://localhost/ecommerce', {
   useNewUrlParser: true,
@@ -15,20 +20,14 @@ mongoose.connect (process.env.MONGODB_URL || 'mongodb://localhost/ecommerce', {
 
 // ---------------------------------------------------------------------
 
-// app.get('/api/products/:id', (req, res)=>{
-//   const product = data.products.find(x => x._id === req.params.id);
-//   if (product) {
-//     res.send(product);
-//   } else {
-//     res.status(404).send({message: 'Product not found'});
-//   }
-// });
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
 // ------------------------------------------------------------------
+//lectura de json request
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
