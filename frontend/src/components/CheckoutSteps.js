1033487703a2794c65029c2a73a5e8d27968ea9e
Andres Canchila <acanchila@intap.com.co>
2021-07-22 13:04:17 -0500

    checkout

---
+++ frontend/src/components/CheckoutSteps.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CheckoutSteps() {
+    return (
+        <div className="row checkout-steps">
+            <div className={props}></div>   
+        </div>
+    )
+}
